c94a607d130374c98c03793cdb26dd7478f9226a
Dan <dangreen.tlv@gmail.com>
2017-06-05 09:46:37 -0400

    Added tests for sorting on the front end

--- wave-frontend/src/components/PayrollReport/PayrollReport.test.js
+++ wave-frontend/src/components/PayrollReport/PayrollReport.test.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import toJson from 'enzyme-to-json';
+import moment from 'moment';
 import {shallow} from 'enzyme';
-import PayrollReport from '.';
 import data from './__testData__/payrollReportData';
+import PayrollReport, {getPayPeriodTimeStamp, sortByPayPeriod} from '.';
 
 test('PayrollReport Renders Correctly', () => {
   const wrapper = shallow(<PayrollReport data={data} />);
   expect(toJson(wrapper)).toMatchSnapshot();
+});
+
+test('Pay period date is extracted correctly', () => {
+  const startDate = '01-01-2016';
+  const payPeriod = `${startDate} - 15-01-2016`;
+  const startDateTimeStamp = moment(startDate, 'DD-MM-YYYY').unix();
+  expect(getPayPeriodTimeStamp({payPeriod})).toEqual(startDateTimeStamp);
+});
+
+test('Pay period is sorting works', () => {
+  const sorted = data.sort(sortByPayPeriod);
+  const lastItem = sorted.pop();
+  expect(lastItem).toMatchObject({
+    employeeId: 4,
+    payPeriod: '16/02/2015 - 28/02/2015',
+    totalPay: 100
+  });
 });
--- wave-frontend/src/components/PayrollReport/index.js
+++ wave-frontend/src/components/PayrollReport/index.js
@@ -4,19 +4,21 @@ import PropTypes from 'prop-types';
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 
-const getGetPayPeriodSortable = ({payPeriod}) => {
+const getPayPeriodTimeStamp = ({payPeriod}) => {
   const [startDate] = payPeriod.split(' - ');
   return moment(startDate, 'DD-MM-YYYY').unix();
 };
+
 const sortByPayPeriod = (a, b, order) => {
-  const A = getGetPayPeriodSortable(a);
-  const B = getGetPayPeriodSortable(b);
+  const A = getPayPeriodTimeStamp(a);
+  const B = getPayPeriodTimeStamp(b);
   if (order === 'desc') {
     return A - B;
   } else {
     return B - A;
   }
 };
+
 class PayrollReport extends Component {
   render() {
     const {data} = this.props;
@@ -45,4 +47,4 @@ PayrollReport.propTypes = {
   data: PropTypes.array.isRequired
 };
 
-export {PayrollReport as default, getGetPayPeriodSortable, sortByPayPeriod}; // Exported for testability
+export {PayrollReport as default, getPayPeriodTimeStamp, sortByPayPeriod}; // Exported for testability
