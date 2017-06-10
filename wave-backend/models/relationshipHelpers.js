814bcdccf2a56bd71a952e1c80bf5fffdcfcb3b3
Dan <dangreen.tlv@gmail.com>
2017-06-10 17:45:45 -0400

    added getHasMany helper

--- wave-backend/models/relationshipHelpers.js
+++ wave-backend/models/relationshipHelpers.js
@@ -9,3 +9,15 @@ module.exports.getBelongsTo = (foreignKey, Model) =>
       });
     });
   };
+
+module.exports.getHasMany = (foreignKey, Model) =>
+  function() {
+    const id = this[foreignKey];
+    const query = {where: {[foreignKey]: id}};
+    return new Promise((resolve, reject) => {
+      Model.all(query, (err, doc) => {
+        if (err) return reject(err);
+        resolve(doc);
+      });
+    });
+  };
